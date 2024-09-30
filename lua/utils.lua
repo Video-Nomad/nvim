efb153de48e98647ad29f02c86be318c841954a2
George Sladkovsky <george.sladkovsky@gmail.com>
2024-09-30 13:37:17 +0200

    Sync

--- lua/utils.lua
+++ lua/utils.lua
@@ -61,18 +61,23 @@ end
 
 function utils:swap_windows(direction)
   local current_win = vim.api.nvim_get_current_win()
+  local current_pos = vim.api.nvim_win_get_cursor(current_win)
   vim.cmd("wincmd " .. direction)
   local target_win = vim.api.nvim_get_current_win()
 
   if current_win ~= target_win then
+    local target_pos = vim.api.nvim_win_get_cursor(target_win)
     local current_buf = vim.api.nvim_win_get_buf(current_win)
     local target_buf = vim.api.nvim_win_get_buf(target_win)
 
+    -- Swap buffers
     vim.api.nvim_win_set_buf(current_win, target_buf)
     vim.api.nvim_win_set_buf(target_win, current_buf)
+
+    -- Swap cursor positions
+    vim.api.nvim_win_set_cursor(current_win, target_pos)
+    vim.api.nvim_win_set_cursor(target_win, current_pos)
 
     vim.api.nvim_set_current_win(target_win)
   end
 end
-
-return utils
