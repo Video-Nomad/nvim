eeda96b7b3813e84927f9ce2468a90b2174b48c0
George Sladkovsky <george.sladkovsky@gmail.com>
2025-01-03 13:15:22 +0100

    Sync

--- lua/plugins/telescope_fzf_native.lua
+++ lua/plugins/telescope_fzf_native.lua
@@ -4,8 +4,16 @@ M.dependencies = {
   "nvim-telescope/telescope.nvim",
 }
 
-M.build =
-  "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+local os_name = vim.loop.os_uname().sysname
+
+if os_name == "Linux" then
+  M.build =
+    "make"
+elseif os_name == "Windows_NT" then
+  M.build =
+    "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+end
+
 
 M.config = function()
   require("telescope").load_extension("fzf")
