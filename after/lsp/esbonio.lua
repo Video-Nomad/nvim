c753275ea504c95d43179c71fbdccc916d8ee167
George Sladkovsky <george.sladkovsky@gmail.com>
2026-01-11 20:36:33 +0100

    Sync

--- after/lsp/esbonio.lua
+++ after/lsp/esbonio.lua
@@ -1,6 +1,35 @@
 ---@type vim.lsp.Config
-return {
-  cmd = { 'esbonio' },
-  filetypes = { 'rst' },
-  root_markers = { '.git' },
+local config = {
+  cmd = { "esbonio" },
+  filetypes = { "rst" },
+  root_markers = { ".git" },
 }
+
+if vim.fn.executable("esbonio") ~= 1 then
+  vim.notify("Esbonio not found. Attempting to install with uv...", vim.log.levels.INFO)
+
+  vim.fn.system({
+    "uv",
+    "tool",
+    "install",
+    "esbonio",
+    "--python",
+    "3.13",
+    "--with",
+    "sphinx",
+    "--with",
+    "sphinx-rtd-theme",
+    "--with",
+    "sphinx-copybutton",
+  })
+
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Failed to install esbonio. Please install it manually.", vim.log.levels.ERROR, {
+      title = "LSP Setup",
+    })
+  else
+    vim.notify("Esbonio installed successfully.", vim.log.levels.INFO)
+  end
+end
+
+return config
